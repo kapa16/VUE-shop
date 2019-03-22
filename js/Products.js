9f5a6f9a3d9c63db383548c1cca972959c298476
Pavel <kapa16@inbox.ru>
2019-03-22 19:29:41 +0500

    correct project

--- js/Products.js
+++ js/Products.js
@@ -32,11 +32,12 @@ Vue.component('product-list', {
         console.log(err);
       });
   },
-  template: `<div class="product__catalog container" >
+  template: `<div class="products catalog-container container" >
                 <div v-for="product of filtered" :key="product.id_product">
                     <product-item :product="product" :img="img" @add-to-cart="$parent.$refs.cart.addToCart"></product-item>
                 </div>
-            </div>`
+            </div>
+`
 });
 
 Vue.component('product-item', {
@@ -44,10 +45,39 @@ Vue.component('product-item', {
     'product',
     'img'
   ],
-  template: `<div class="product__card">
-                <img :src="img" alt="photo">
-                <h3>{{product.product_name}}</h3>
-                <p>Цена: {{product.price}}</p>
-                <button @click="$emit('add-to-cart',product)" class="btn product__btn">КУПИТЬ</button>
-            </div>`
+  // language=HTML
+  template: `
+<div class="product-card" data-id="9" data-name="Mango People T-shirt" data-price="52">
+    <a class="product-card-link" href="single-page.html">
+        <div class="product-img-wrap">
+            <img class="product-img" :src="img" :alt="product.product_name">
+            <div class="product-img-mask"></div>
+        </div>
+
+        <div class="product-card-info">
+            <p class="product-name">{{product.product_name}}</p>
+
+            <div class="product-bottom">
+                <p class="product-price">$ {{product.price}}</p>
+                <div class="product-rating">
+                    <i class="fas fa-star rating-star"></i>
+                    <i class="fas fa-star rating-star"></i>
+                    <i class="fas fa-star rating-star"></i>
+                    <i class="fas fa-star rating-star"></i>
+                    <i class="far fa-star rating-star"></i>
+                </div>
+            </div>
+        </div>
+    </a>
+    <div class="parent-product-card">
+        <div class="add-cart-wrap" @click="$emit('add-to-cart',product)">
+            <a href="#" class="product-card-hover">Add to Cart</a>
+        </div>
+        <div class="product-card-hover-buttons">
+            <a href="#" class="product-card-hover reload"></a>
+            <a href="#" class="product-card-hover favorite"></a>
+        </div>
+    </div>
+</div>
+`
 });
